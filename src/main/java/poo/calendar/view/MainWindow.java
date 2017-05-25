3a5cfad51f2503c1f2fa71f55904c44635c01d32
Matheus Saldanha <matheus.saldanha@usp.br>
2017-05-25 16:09:06 -0300

    Add JavaFX to Maven project and helpful file

--- src/main/java/poo/calendar/view/MainWindow.java
+++ src/main/java/poo/calendar/view/MainWindow.java
@@ -1,27 +1,37 @@
 package poo.calendar.view;
 
-import java.awt.EventQueue;
-import javax.swing.JFrame;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class MainWindow extends JFrame {
+public class MainWindow extends Application {
     /** Eclipse force this variable to exist. */
 	private static final long serialVersionUID = 9110319843067855971L;
 
-	public MainWindow(){
-    	initUI();
+	public static void main(String args[]){
+    	launch(args);
     }
 	
-	private void initUI() {
-        setTitle("Simple example");
-        setSize(300, 200);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-    }
-
-    public static void main(String[] args) {
-        EventQueue.invokeLater(() -> {
-            MainWindow ex = new MainWindow();
-            ex.setVisible(true);
+	@Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
         });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 }
