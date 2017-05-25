c317193fa0a065c41ae333014805f90471991e21
Carlos Neves <carlos.neves@usp.br>
2017-05-25 16:01:39 +0000

    Task and View classes.

--- src/poo/calendar/model/Task.java
+++ src/poo/calendar/model/Task.java
@@ -2,13 +2,65 @@ package poo.calendar.model;
 
 import java.util.Calendar;
 
+/**
+ *
+ * A model class that represents a Task. A task has a title(required) and a
+ * deadline date(optional).
+ *
+ * @author neves
+ *
+ */
 public class Task {
-	private String d_title;
-	private Calendar d_date;
-	
-	public Task(String title, Calendar date) throws IllegalArgumentException {
-		d_title = title;
-		d_date = date;
-		//TODO: validate date
+
+	/** The task's title. */
+	private String mTitle;
+
+	/** The task's deadline date. */
+	private Calendar mDeadlineDate;
+
+	/**
+	 *
+	 * The task's default constructor.
+	 *
+	 * @param title required
+	 * @param deadlineDate optional
+	 * @throws IllegalArgumentException
+	 */
+	public Task(String title, Calendar deadlineDate) throws IllegalArgumentException {
+		if (title == null || title.equals("")) {
+			throw new IllegalArgumentException("A task must provide a non-null non-empty title string.");
+		}
+
+		mTitle = title;
+		mDeadlineDate = deadlineDate;
 	}
+
+	/**
+	 * @return the task's title
+	 */
+	public String getTitle() {
+		return mTitle;
+	}
+
+	/**
+	 * @param mTitle the task's title to set
+	 */
+	public void setTitle(String title) {
+		mTitle = title;
+	}
+
+	/**
+	 * @return the task's deadline date
+	 */
+	public Calendar getDeadlineDate() {
+		return mDeadlineDate;
+	}
+
+	/**
+	 * @param mDeadlineDate the task's deadline date to set
+	 */
+	public void setD_date(Calendar deadlineDate) {
+		mDeadlineDate = deadlineDate;
+	}
+
 }
