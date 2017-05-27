0739d6dad1ddccfe402d511fc98e76c71d7dbe94
Matheus Saldanha <matheus.saldanha@usp.br>
2017-05-27 19:29:24 -0300

    Add information to MAVEN_INFO

--- src/main/java/poo/calendar/view/MainWindow.java
+++ src/main/java/poo/calendar/view/MainWindow.java
@@ -19,7 +19,6 @@ public class MainWindow extends Application {
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
             }
