44fab9bef2164c74ef87509e6711555346d52ab6
Matheus Saldanha <matheus.saldanha@usp.br>
2017-06-01 09:16:30 -0300

    Add javadocs to IDGenerator

--- src/poo/calendar/model/IDGenerator.java
+++ src/poo/calendar/model/IDGenerator.java
@@ -1,10 +1,17 @@
 package poo.calendar.model;
 
+/**
+ * Class for handling generation of unique IDs along the program
+ */
 public class IDGenerator {
 	private static long mCounter = 0;
 	
+	// Prevent instantiation
 	private IDGenerator(){}
 	
+	/**
+	 * @return a unique identifier number.
+	 */
 	public static long getID(){
 		long id = mCounter;
 		mCounter++;
