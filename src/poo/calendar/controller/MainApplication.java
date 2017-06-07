a02d3295b668f0271536ea2396fd8bcf3e01511a
Matheus Saldanha <matheus.saldanha@usp.br>
2017-06-07 17:12:52 -0300

    First full implementation of CalendarGroup

--- src/poo/calendar/controller/MainApplication.java
+++ src/poo/calendar/controller/MainApplication.java
@@ -1,24 +1,28 @@
 package poo.calendar.controller;
 
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableSet;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import poo.calendar.model.Appointment;
+import poo.calendar.model.CalendarGroup;
 import poo.calendar.model.Task;
 import poo.calendar.view.MainScene;
 
 public class MainApplication extends Application {
 	private ObservableList<Appointment> mAppointments;
 	private ObservableList<Task> mTasks;
-
+	private ObservableSet<CalendarGroup> mGroups;
+	
 	@Override
 	public void start(Stage stage) {
 		// TODO: read application data from a persistent storage.
 		mAppointments = FXCollections.observableArrayList();
 		mTasks = FXCollections.observableArrayList();
-
+		mGroups = FXCollections.observableSet();
+		
 		AppointmentController.getInstance().initializeModel(mAppointments);
 		TaskController.getInstance().initializeModel(mTasks);
 
--- src/poo/calendar/model/CalendarGroup.java
+++ src/poo/calendar/model/CalendarGroup.java
@@ -1,5 +1,11 @@
 package poo.calendar.model;
 
+import java.util.UUID;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.scene.paint.Color;
 
 /**
@@ -9,7 +15,7 @@ import javafx.scene.paint.Color;
  */
 public class CalendarGroup {
 	// ID for the default group
-	public static Integer DEFAULT_ID = 0;
+	public static UUID DEFAULT_ID = UUID.fromString("d26a05c8-60c7-4a15-86ce-12605bb198a8");
 	
 	// Array of colors that CalendarGroups can assume
 	private static Color[] mColorArray = new Color[] {
@@ -19,16 +25,76 @@ public class CalendarGroup {
 	};
 	
 	//The group's name
-	private String mName;
+	private StringProperty mName;
 	
 	//The group's color
-	private Color mColor;
+	private ObjectProperty<Color> mColor;
 	
 	//The group's ID
-	private Integer mID;
+	private UUID mID;
 	
-	public CalendarGroup(String name, Color mColor, Integer id){
+	/**
+	 * Default constructor.
+	 * @param name The group's name
+	 * @param mColor The group's color
+	 * @throws NullPointerException if any argument is null
+	 */
+	public CalendarGroup(String name, Color color) throws NullPointerException {
+		mName = new SimpleStringProperty();
+		mColor = new SimpleObjectProperty<Color>();
 		
+		this.setName(name);
+		this.setColor(color);
+		
+		mID = UUID.randomUUID();
+	}
+	
+	/**
+	 * @return the group's name
+	 */
+	public final String getName(){
+		return mName.get();
+	}
+	
+	/**
+	 * @param name new name for the group
+	 * @throws NullPointerException if parameter 'name' is null
+	 */
+	public final void setName(String name) throws NullPointerException {
+		if(name == null)
+			throw new NullPointerException("CalendarGroup's name cannot be null");
+		mName.set(name);
+	}
+	
+	/**
+	 * @return the group's name property.
+	 */
+	public StringProperty nameProperty(){
+		return mName;
+	}
+	
+	/**
+	 * @return the group's color.
+	 */
+	public final Color getColor(){
+		return mColor.get();
+	}
+	
+	/**
+	 * @param col new color for the group
+	 * @throws NullPointerException if given parameter is null
+	 */
+	public final void setColor(Color col) throws NullPointerException {
+		if(col == null)
+			throw new NullPointerException("CalendarGroup's color cannot be null");
+		mColor.set(col);
+	}
+	
+	/**
+	 * @return the color property of the group
+	 */
+	public ObjectProperty<Color> colorProperty(){
+		return mColor;
 	}
 	
 	@Override
