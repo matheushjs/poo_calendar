9a9f81c430b198053fe26e2cd63c9d95afa71a06
Matheus Saldanha <matheus.saldanha@usp.br>
2017-06-09 18:40:56 -0300

    Add basic task and appointment dialogs

---
+++ src/poo/calendar/dialogscenes/AppointmentDialogController.java
@@ -0,0 +1,81 @@
+package poo.calendar.dialogscenes;
+
+import java.util.UUID;
+
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
+import javafx.scene.text.Text;
+import poo.calendar.controller.MainApplication;
+import poo.calendar.model.Appointment;
+import poo.calendar.model.CalendarGroup;
+import poo.calendar.model.Task;
+
+
+/**
+ * Class for controlling the dialog for managing appointments.
+ * If not provided an appointment ID, the dialog is run on CREATE mode.
+ * If provided an ID, run on EDIT mode.
+ */
+public class AppointmentDialogController {
+	@FXML
+	private DialogPane mMainPane;
+	
+	@FXML
+	private Text mHeaderText;
+	
+	private MainApplication mMainApp;
+	
+	// Model data
+	public ObservableMap<UUID, CalendarGroup> mGroupMap;
+	public ObservableList<Task> mTasks;
+	public ObservableList<Appointment> mAppointments;
+	public UUID mAppointmentID;
+	
+	/**
+	 * Default constructor
+	 */
+	public AppointmentDialogController(){
+	}
+	
+	/**
+	 * Called by FXML -after- the .fxml is loaded.
+	 */
+	@FXML
+	private void initialize(){
+		//ŦODO: Connect due signals
+	}
+	
+	/**
+	 * Receives the map of groups to which the new group will be added.
+	 */
+	public void initializeModel(ObservableMap<UUID, CalendarGroup> map, ObservableList<Task> tasks, ObservableList<Appointment> appts){
+		mGroupMap = map;
+		mTasks = tasks;
+		mAppointments = appts;
+	}
+	
+	/**
+	 * Sets the main application from which this dialog will later request a scene change;
+	 * @param app
+	 */
+	public void setMainApp(MainApplication app){
+		mMainApp = app;
+		
+		Button bt = (Button) mMainPane.lookupButton(ButtonType.APPLY);
+		bt.setOnAction(action -> mMainApp.displayMainRoot());
+	}
+	
+	/**
+	 * Sets the dialog to EDIT mode, to edit the appointment identified by 'id'.
+	 * @param id
+	 */
+	public void setAppointmentID(UUID id){
+		mAppointmentID = id;
+		//TODO: Place appointment info in the UI
+		//TODO: configure UI to edit mode
+	}
+}
