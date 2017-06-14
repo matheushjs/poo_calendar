03f01e9e31ea5c2d0bb25074be796c761f9e5b10
Matheus Saldanha <matheus.saldanha@usp.br>
2017-06-13 22:48:33 -0300

    Add some DateUtil functions

--- src/poo/calendar/DateUtil.java
+++ src/poo/calendar/DateUtil.java
@@ -5,6 +5,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DateUtil {
+	public static int MINUTES_IN_DAY = 24 * 60;
 	
 	/**
 	 * Creates a calendar from 2 strings 'date' and 'hour'.
@@ -75,6 +76,18 @@ public class DateUtil {
 	}
 	
 	/**
+	 * Retrieves a hour:minute string from the given amount of minutes elapsed in the day.
+	 * @param minutes
+	 * @return
+	 */
+	public static String hourString(int minutes){
+		Calendar c = Calendar.getInstance();
+		c.set(Calendar.HOUR, minutes / 60);
+		c.set(Calendar.MINUTE, minutes % 60);
+		return hourString(c);
+	}
+	
+	/**
 	 * Sets fields lower than DATE of 'calendar' to 0. 
 	 * @param calendar
 	 */
@@ -95,13 +108,162 @@ public class DateUtil {
 		calendar.set(Calendar.DAY_OF_WEEK, calendar.getFirstDayOfWeek());
 	}
 	
+	/**
+	 * Given an interval between 2 dates, translate the interval so that the
+	 * 'field' of the initial date  is the same as the 'field' of the given 'target' date.
+	 * All fields higher than 'field' are translated also.
+	 * 
+	 * Supported values for 'field' are Calendar's YEAR, MONTH, DATE and DAY_OF_WEEK.
+	 * 
+	 * The given 'init' and 'end' calendars are effectively altered, so cloning might be necessary.
+	 * 
+	 * @param target Base date
+	 * @param init Initial date of the interval to be translated
+	 * @param end End date of the interval to be translated
+	 * @throws IllegalArgumentException if translation related to the given 'field' is not supported.
+	 */
+	public static void translateInterval(int field, Calendar target, Calendar init, Calendar end) throws IllegalArgumentException {
+		if(field != Calendar.YEAR && field != Calendar.MONTH && field != Calendar.DATE && field != Calendar.DAY_OF_WEEK)
+			throw new IllegalArgumentException("Unsupported calendar translation field");
+		
+		boolean translateWeekday;
+		int savedWeekDay = 0;
+
+		if(field == Calendar.DAY_OF_WEEK){	
+			translateWeekday = true;
+			savedWeekDay = init.get(Calendar.DAY_OF_WEEK);
+			field = Calendar.DATE;
+		} else {
+			translateWeekday = false;
+		}
+		
+		// Save states
+		boolean initLeniency, endLeniency;
+		initLeniency = init.isLenient();
+		endLeniency = end.isLenient();
+		
+		// Change states
+		init.setLenient(true);
+		end.setLenient(true);
+		
+		int[] fields = new int[] { Calendar.YEAR, Calendar.MONTH, Calendar.DATE };
+		int[] deltas = new int[3];
+		
+		// Calculate needed offsets
+		for(int i = 0; i < 3; i++)
+			deltas[i] = end.get(fields[i]) - init.get(fields[i]);
+		
+		// Translate initial date
+		for(int i = 0; i < 3; i++){
+			init.set(fields[i], target.get(fields[i]));
+			
+			if(fields[i] == field) break;
+		}
+		
+		// Translate end date by applying offsets in the new init date.
+		// Inverted order to prevent wrong rounding in the day number.
+		for(int i = 2; i >= 0; i--){
+			end.set(fields[i], init.get(fields[i]));
+			end.add(fields[i], deltas[i]);
+		}
+		
+		// Translate to DAY_OF_WEEK if needed
+		int previous, delta;
+		if(translateWeekday){
+			// Set initial date to the correct DAY_OF_WEEK
+			previous = init.get(Calendar.DATE);
+			init.set(Calendar.DAY_OF_WEEK, savedWeekDay);
+			
+			// Calculate the variation
+			delta = init.get(Calendar.DATE) - previous;
+			
+			// Apply variation to the end date.
+			end.add(Calendar.DATE, delta);
+		}
+		
+		// Restore states
+		init.setLenient(initLeniency);
+		end.setLenient(endLeniency);
+	}
+	
+	/**
+	 * Verifies if the interval [init, end] has intersection with the interval [lowerBound, upperBound].
+	 * Intersection is checked to all fields higher than or equal to DATE. This means HOUR and MINUTE for
+	 * example are ignored.
+	 * 
+	 * @param lowerBound
+	 * @param upperBound
+	 * @param init
+	 * @param end
+	 * @return TRUE if intersection exists.
+	 */
+	public static boolean hasDayIntersection(Calendar lowerBound, Calendar upperBound, Calendar init, Calendar end){
+		int[] fields = new int[] { Calendar.YEAR, Calendar.MONTH, Calendar.DATE };
+		
+		for(int i = 0; i < 3; i++){
+			if( init.get(fields[i]) > upperBound.get(fields[i]) )
+				return false;
+			if( lowerBound.get(fields[i]) > end.get(fields[i]) )
+				return false;
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * Verifies if the interval [init, end] contains 'subjectDay', in the same fashion as the other
+	 * hasDayIntersection() method.
+	 * 
+	 * Remember that HOUR, MINUTE and any lower fields are ignored.
+	 * 
+	 * @param subjectDay
+	 * @param init
+	 * @param end
+	 * @return if 'subjectDay' is within the interval [init, end]
+	 */
+	public static boolean hasDayIntersection(Calendar subjectDay, Calendar init, Calendar end){
+		return hasDayIntersection(subjectDay, subjectDay, init, end);
+	}
+	
+	/**
+	 * @return 60 * HOUR + MINUTES   of the given calendar.
+	 */
+	public static int minuteCount(Calendar calendar){
+		return 60*calendar.get(Calendar.HOUR) + calendar.get(Calendar.MINUTE);
+	}
+	
+	/**
+	 * Verifies if 'maybeOffset' has the same date as today.add(DATE, offset)
+	 * 
+	 * @param today
+	 * @param maybeOffset
+	 * @param offset
+	 * @return
+	 */
+	public static boolean isDayOffset(Calendar today, Calendar maybeOffset, int offset){
+		int[] fields = new int[] { Calendar.YEAR, Calendar.MONTH, Calendar.DATE };
+		
+		// Clone so as to not change the user's calendar.
+		Calendar key = (Calendar) today.clone();
+		key.add(Calendar.DATE, offset);
+		
+		for(int i = 0; i < 2; i++)
+			if(key.get(fields[i]) != maybeOffset.get(fields[i]))
+				return false;
+		
+		return true;
+	}
+	
 	/*
 	public static void main(String[] args){
-		Calendar c = Calendar.getInstance();
+		Calendar a = parseFields("26/06/2017", "1:05");
+		Calendar b = parseFields("27/06/2017", "1:35");
+		Calendar c = parseFields("14/06/2017", "00:00");
+
+		DateUtil.translateInterval(Calendar.DAY_OF_WEEK, c, a, b);
 		
-		c.add(Calendar.DAY_OF_MONTH, -2);
-		DateUtil.resetToWeek(c);
-		System.out.println(dateString(c));
-	}
-	*/
+		System.out.println(dateString(a) + " " + hourString(a));
+		System.out.println(dateString(b) + " " + hourString(b));
+		System.out.println(dateString(c) + " " + hourString(c));
+	}*/
 }
