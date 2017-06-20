6874f3eb42240dbb015e5f5240f002bc7e58a423
Matheus Saldanha <matheus.saldanha@usp.br>
2017-06-19 22:22:32 -0300

    Make DateTextAnimation a private class

--- src/poo/calendar/widgets/DateText.java
+++ src/poo/calendar/widgets/DateText.java
@@ -4,10 +4,13 @@ import java.text.DateFormatSymbols;
 import java.util.Calendar;
 import java.util.Locale;
 
+import javafx.animation.AnimationTimer;
+import javafx.animation.FadeTransition;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontSmoothingType;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 /**
  * Text widget that displays a date.
@@ -44,5 +47,61 @@ public class DateText extends Text {
 	 */
 	public void update(){
 		setText(makeString());
+	}
+	
+	/**
+	 * Class exclusively for animating the DateText widget.
+	 * It's currently exclusive for the DateText class, hence why it's nested within it and declared private.
+	 */
+	private class DateTextAnimation extends AnimationTimer {
+		
+		private DateText date;
+		
+		/**
+		 * Default constructor
+		 * @param dateText the text to animate
+		 */
+		public DateTextAnimation(DateText dateText){
+			date = dateText;
+			this.start();
+		}
+
+		@Override
+		public void handle(long now) {
+			FadeTransition ft = new FadeTransition(Duration.millis(1000), date);		
+			if(fadeOut()){
+				ft.setFromValue(1.0);
+				ft.setToValue(0.0);
+				ft.setCycleCount(1);
+				ft.playFromStart();
+			}else if(fadeIn()){
+				date.update();
+				ft.setFromValue(0.0);
+				ft.setToValue(1.0);
+				ft.setCycleCount(1);
+				ft.playFromStart();
+			}
+			
+		}
+		
+		private boolean fadeOut(){
+			return checkMidnight();
+		}
+		
+		private boolean fadeIn(){
+			Calendar cal = Calendar.getInstance();
+			if(cal.get(Calendar.HOUR) != 0) return false;
+			if(cal.get(Calendar.MINUTE) != 0) return false;
+			if(cal.get(Calendar.SECOND) != 2) return false;
+			return true;
+		}
+		
+		private boolean checkMidnight(){
+			Calendar cal = Calendar.getInstance();
+			if(cal.get(Calendar.HOUR) != 0) return false;
+			if(cal.get(Calendar.MINUTE) != 0) return false;
+			if(cal.get(Calendar.SECOND) != 0) return false;
+			return true;
+		}
 	}
 }
